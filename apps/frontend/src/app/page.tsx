dadb2d4c8d7dd3bb78318a507b78dc0be615945d
mallory-scotton <mallory.scotton@epitech.eu>
2026-06-05 16:59:35 +0200

    add: implement shimmer animation for enhanced text effect

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -26,7 +26,7 @@ export default function Home() {
           <div className='absolute inset-0 z-0 bg-[radial-gradient(#00000020_1px,transparent_1px)] bg-size-[20px_20px] mask-[linear-gradient(to_bottom,transparent_0%,black_10%,black_80%,transparent_100%)] pointer-events-none'></div>
 
           <div className='relative z-10 flex flex-col items-center w-full'>
-            <p className='text-blue-500 text-center text-base font-medium leading-[normal] max-md:text-sm'>
+            <p className='text-center text-lg font-medium leading-[normal] max-md:text-sm inline-block bg-size-[200%_100%] bg-linear-to-r from-blue-500 via-blue-200 to-blue-500 bg-clip-text text-transparent animate-shimmer'>
               Graphical Playground is now live
             </p>
             <h1 className='mx-auto mt-6 max-w-5xl text-center text-4xl sm:text-5xl md:text-6xl lg:text-[72px] font-normal leading-tight lg:leading-none tracking-[-0.02em] text-black'>
