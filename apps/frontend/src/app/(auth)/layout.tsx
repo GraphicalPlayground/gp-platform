caf706c9b62d0e5c24e07ed3549794673ed04d99
mallory-scotton <mallory.scotton@epitech.eu>
2026-06-10 12:20:00 +0200

    feat: add authentication pages for forgot password, login, register, and reset password

---
+++ apps/frontend/src/app/(auth)/layout.tsx
@@ -0,0 +1,15 @@
+// Copyright (c) - Graphical Playground. All rights reserved.
+// For more information, see https://graphical-playground/legal
+// mailto:support AT graphical-playground DOT com
+
+'use client';
+
+import React from 'react';
+
+export default function AuthLayout({
+  children
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return <>{children}</>;
+}
