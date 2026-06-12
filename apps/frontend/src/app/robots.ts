49572713eb56facc15383777663bfe13389954d2
mallory-scotton <mallory.scotton@epitech.eu>
2026-06-12 15:30:47 +0200

    feat: add robots.txt generation functionality for SEO optimization

---
+++ apps/frontend/src/app/robots.ts
@@ -0,0 +1,21 @@
+// Copyright (c) - Graphical Playground. All rights reserved.
+// For more information, see https://graphical-playground/legal
+// mailto:support AT graphical-playground DOT com
+
+import { siteConfig } from '@/config/site';
+import type { MetadataRoute } from 'next';
+
+/**
+ * @brief This function generates the robots.txt content for the website.
+ * It allows all user agents to access the entire site and specifies the location of the sitemap.
+ * @returns An object representing the robots.txt rules and sitemap location.
+ */
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/'
+    },
+    sitemap: `${siteConfig.links.website}/sitemap.xml`
+  };
+}
