00f8de50b4ea56088959ba03ba2d4e0d2207aac8
mallory-scotton <mallory.scotton@epitech.eu>
2026-06-15 16:40:34 +0200

    feat: enhance MDX components with scroll margin for improved accessibility

--- apps/frontend/src/mdx-components.tsx
+++ apps/frontend/src/mdx-components.tsx
@@ -13,10 +13,30 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
         {children}
       </MdxLink>
     ),
-    h1: ({ children }) => <h1 className='text-6xl mb-4'>{children}</h1>,
-    h2: ({ children }) => <h2 className='text-4xl mb-4 mt-12'>{children}</h2>,
-    h3: ({ children }) => <h3 className='text-2xl mb-4 mt-6'>{children}</h3>,
-    h4: ({ children }) => <h4 className='text-xl mb-4 mt-4'>{children}</h4>,
-    p: ({ children }) => <p className='text-base xl:text-lg leading-[1.6] my-4 mb-4'>{children}</p>
+    h1: ({ children, ...props }) => (
+      <h1 className='text-6xl mb-4 scroll-mt-24' {...props}>
+        {children}
+      </h1>
+    ),
+    h2: ({ children, ...props }) => (
+      <h2 className='text-4xl mb-4 mt-12 scroll-mt-24' {...props}>
+        {children}
+      </h2>
+    ),
+    h3: ({ children, ...props }) => (
+      <h3 className='text-2xl mb-4 mt-6 scroll-mt-24' {...props}>
+        {children}
+      </h3>
+    ),
+    h4: ({ children, ...props }) => (
+      <h4 className='text-xl mb-4 mt-4 scroll-mt-24' {...props}>
+        {children}
+      </h4>
+    ),
+    p: ({ children, ...props }) => (
+      <p className='text-base xl:text-lg leading-[1.6] my-4 mb-4' {...props}>
+        {children}
+      </p>
+    )
   };
 }
