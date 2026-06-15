9cf009f162642ab5f4dfcdd5e14852829df2e330
mallory-scotton <mallory.scotton@epitech.eu>
2026-06-15 17:02:27 +0200

    feat: enhance LegalLayout with header and logo for improved branding

--- apps/frontend/src/app/(marketing)/legal/layout.tsx
+++ apps/frontend/src/app/(marketing)/legal/layout.tsx
@@ -43,9 +43,27 @@ export default function LegalLayout({
 }>) {
   return (
     <div className='w-full flex flex-col'>
+      <header className='w-full bg-[url("/images/dithered/sphere-dithered.png")] bg-center bg-no-repeat bg-cover h-64 md:h-96 flex flex-col items-center justify-center gap-3'>
+        <div className='w-[90%] max-w-[20rem] md:max-w-none md:w-1/2 lg:w-1/3 xl:w-1/4 flex flex-col items-center justify-center gap-4 md:gap-5'>
+          <div className='w-full flex items-center justify-center bg-(--gp-primary) px-6 py-4 md:px-8 md:py-6 rounded-3xl md:rounded-4xl'>
+            <img
+              src='/images/logo-long-text-black.svg'
+              alt='Graphical Playground Logo'
+              className='w-full h-auto'
+              draggable={false}
+            />
+          </div>
+
+          <div className='w-full px-4 md:px-6 flex justify-center'>
+            <span className='bg-(--gp-pink) text-black text-2xl md:text-4xl px-5 md:px-6 pt-1.5 md:pt-2.5 pb-1 md:pb-2 rounded-full'>
+              Legal
+            </span>
+          </div>
+        </div>
+      </header>
       <main className='w-[calc(min(var(--max-content-width),100vw)-var(--gutter)*2)] mx-auto py-20'>
         <div className='min-[960px]:grid min-[960px]:gap-[calc(var(--col-width)*3)] min-[960px]:grid-cols-[calc(var(--col-width)*14)_calc(var(--col-width)*27)] min-[1440px]:grid-cols-[calc(var(--col-width)*12)_calc(var(--col-width)*26)] min-[1920px]:grid-cols-[calc(var(--col-width)*13)_calc(var(--col-width)*24)]'>
-          <aside className='w-full mb-12 md:mt-4'>
+          <aside className='w-full mb-12 md:mt-4 min-[960px]:sticky min-[960px]:top-32 self-start'>
             <LegalNav />
           </aside>
           <div className='w-full min-h-[50vh]'>{children}</div>
