fb439df08cc7644ffabca3944bb737cb2300578b
mallory-scotton <mallory.scotton@epitech.eu>
2026-06-16 10:50:19 +0200

    fix: correct animation style syntax and adjust layout in CallToActionSection

--- apps/frontend/src/app/(marketing)/components/sections/call-to-action.tsx
+++ apps/frontend/src/app/(marketing)/components/sections/call-to-action.tsx
@@ -15,11 +15,11 @@ export const CallToActionSection = () => {
           <div className='sticky bottom-0 w-full z-10'>
             <FooterCallToAction title='Start learning today' />
           </div>
-          <div className='absolute flex flex-col w-full h-full justify-center text-[max(0.5px,0.0625*var(--scaling-factor))] ml-[3.5vw]'>
+          <div className='absolute flex flex-col h-full justify-center text-[max(0.5px,0.0625*var(--scaling-factor))] ml-[3.5vw]'>
             <p>Unleash your imagination.</p>
             <p
               className='bg-size-[200%_100%] bg-linear-to-r from-black via-(--gp-pink) to-black bg-clip-text text-transparent'
-              style={{ animation: '7.5s linear infinite shimmer;' }}
+              style={{ animation: '7.5s linear infinite shimmer' }}
             >
               Master your craft.
             </p>
