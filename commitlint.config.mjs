2db97b0e92e1d36171187d8161641b02475c638d
mallory-scotton <mscotton.pro@gmail.com>
2026-07-23 15:07:31 +0200

    fix: fixing missing license header

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,3 +1,7 @@
+// Copyright (c) - Graphical Playground. All rights reserved.
+// For more information, see https://graphical-playground/legal
+// mailto:support AT graphical-playground DOT com
+
 import conventional from '@commitlint/config-conventional';
 
 /**
--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,3 +1,7 @@
+// Copyright (c) - Graphical Playground. All rights reserved.
+// For more information, see https://graphical-playground/legal
+// mailto:support AT graphical-playground DOT com
+
 import fs from 'fs';
 import { dirname, relative, resolve } from 'path';
 
