3687f464f9b2eaa68eed9fd4d368d5b91e71ce23
mallory-scotton <mallory.scotton@epitech.eu>
2026-07-25 11:53:37 +0200

    add: extend SoftwareSlug type and update JsonLdIds for software source code and application links

--- packages/seo/src/jsonld/ids.ts
+++ packages/seo/src/jsonld/ids.ts
@@ -48,6 +48,7 @@ export const Urls = {
 export type ContactSlug = 'support' | 'legal' | 'press' | 'sales' | 'security';
 export type LanguageSlug = 'en' | 'fr';
 export type PersonSlug = 'mallory-scotton' | 'hugo-cathelain' | 'ossan-msoili' | 'raphael-ostier' | 'nathan-fievet';
+export type SoftwareSlug = 'gp-platform' | 'gp-engine' | 'gp-docs';
 
 /**
  * @brief A collection of JSON-LD IDs for various entities used in the Graphical Playground platform.
@@ -56,9 +57,10 @@ export const JsonLdIds = {
   organization: `${Urls.BaseUrl}/#organization`,
   website: `${Urls.BaseUrl}/#website`,
   person: (slug: PersonSlug) => `${Urls.BaseUrl}/team/${slug}#person`,
-  software: `${Urls.BaseUrl}/#software`,
   logo: `${Urls.BaseUrl}/#logo`,
   localization: `${Urls.BaseUrl}/#localization`,
   language: (lang: LanguageSlug) => `${Urls.BaseUrl}/#language-${lang}`,
-  contact: (slug: ContactSlug) => `${Urls.BaseUrl}/#contact-${slug}`
+  contact: (slug: ContactSlug) => `${Urls.BaseUrl}/#contact-${slug}`,
+  sourceCode: (slug: SoftwareSlug) => `${Urls.BaseUrl}/#software-source-code-${slug}`,
+  application: (slug: SoftwareSlug) => `${Urls.BaseUrl}/#software-application-${slug}`
 } as const;
