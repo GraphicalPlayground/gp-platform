62097be35b835c69f87d748f67f3df4bab874e20
mallory-scotton <mallory.scotton@epitech.eu>
2026-07-25 16:32:26 +0200

    add: create new pages for various articles and sections in the marketing app

---
+++ apps/marketing/src/app/(articles)/showcases/[slug]/page.tsx
@@ -0,0 +1,9 @@
+// Copyright (c) - Graphical Playground. All rights reserved.
+// For more information, see https://graphical-playground/legal
+// mailto:support AT graphical-playground DOT com
+
+import React from 'react';
+
+export default function ShowcaseSlugPage() {
+  return <div>Showcase Slug</div>;
+}
