718008293d2ead2402f12ae25f825dc0d9e423ae
mallory-scotton <mallory.scotton@epitech.eu>
2026-07-25 11:15:43 +0200

    add: implement JsonLd component for rendering JSON-LD data

---
+++ packages/seo/src/components/JsonLd.tsx
@@ -0,0 +1,42 @@
+// Copyright (c) - Graphical Playground. All rights reserved.
+// For more information, see https://graphical-playground/legal
+// mailto:support AT graphical-playground DOT com
+
+import React from 'react';
+import type { Thing, WithContext, Graph } from 'schema-dts';
+
+/**
+ * @brief Props for the JsonLd component.
+ */
+export interface JsonLdProps<T extends Thing> extends Omit<
+  React.HTMLAttributes<HTMLScriptElement>,
+  'children' | 'src' | 'dangerouslySetInnerHTML'
+> {
+  /**
+   * @brief The JSON-LD data to be rendered as a string.
+   */
+  data: WithContext<T> | Graph | WithContext<T>[];
+
+  /**
+   * @brief The number of spaces to use for indentation in the JSON-LD string.
+   * @default 2
+   */
+  indent?: number;
+}
+
+/**
+ * @brief A React component that renders JSON-LD data as a script tag.
+ * @details This component is XSS-safe and prevents the execution of any scripts within the JSON-LD data.
+ */
+export const JsonLd = <T extends Thing>({ data, indent = 2, ...rest }: JsonLdProps<T>) => {
+  if (Array.isArray(data)) {
+    const graphData: Graph = { '@context': 'https://schema.org', '@graph': data };
+    const jsonString = JSON.stringify(graphData, null, indent);
+    const xssSafeData = jsonString.replace(/</g, '\\u003c').replace(/>/g, '\\u003e');
+    return <script type='application/ld+json' dangerouslySetInnerHTML={{ __html: xssSafeData }} {...rest} />;
+  }
+
+  const jsonString = JSON.stringify(data, null, indent);
+  const xssSafeData = jsonString.replace(/</g, '\\u003c').replace(/>/g, '\\u003e');
+  return <script type='application/ld+json' dangerouslySetInnerHTML={{ __html: xssSafeData }} {...rest} />;
+};
