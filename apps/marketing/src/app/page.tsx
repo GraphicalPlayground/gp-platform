8185159e990319f4c279ef7a28a98f9cd0280612
mallory-scotton <mallory.scotton@epitech.eu>
2026-07-26 23:26:10 +0200

    add: adding little test of the text components in the page

--- apps/marketing/src/app/page.tsx
+++ apps/marketing/src/app/page.tsx
@@ -12,7 +12,22 @@ import { Text } from '@gp/ui/components';
 export default function HomePage() {
   return (
     <div>
-      <Text>Hello World, this is a text !</Text>
+      <div>
+        <h2>Aligned Text</h2>
+        <div className='flex w-100 flex-col border'>
+          <Text align='start'>Start aligned text</Text>
+          <Text align='center'>Center aligned text</Text>
+          <Text align='end'>End aligned text</Text>
+        </div>
+      </div>
+      <div>
+        <h2>Font Familly</h2>
+        <div className='flex w-100 flex-col border'>
+          <Text font='mona-sans'>Mona Sans</Text>
+          <Text font='hubot-sans'>Hubot Sans</Text>
+          <Text font='monospace'>Monospace</Text>
+        </div>
+      </div>
     </div>
   );
 }
