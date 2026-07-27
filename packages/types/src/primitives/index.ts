96a88e33fe0564ef2dbf6af5ad27a0057e8bdcdf
mallory-scotton <mscotton.pro@gmail.com>
2026-07-27 10:48:33 +0200

    refactor: reorder exports in index.ts for improved organization

--- packages/types/src/primitives/index.ts
+++ packages/types/src/primitives/index.ts
@@ -2,17 +2,28 @@
 // For more information, see https://graphical-playground/legal
 // mailto:support AT graphical-playground DOT com
 
+export * from './aspect-ratio';
+export * from './byte-size';
 export * from './color';
 export * from './country';
 export * from './currency';
 export * from './date';
+export * from './direction';
 export * from './duration';
 export * from './email';
+export * from './geo';
 export * from './language';
 export * from './locale';
+export * from './mime';
+export * from './money';
+export * from './percentage';
 export * from './phone';
+export * from './progress';
 export * from './pronouns';
 export * from './rating';
+export * from './semver';
 export * from './slug';
 export * from './social';
+export * from './timezone';
 export * from './url';
+export * from './uuid';
