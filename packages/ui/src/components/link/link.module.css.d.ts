e0e04d564440cdca4b22ee662b6862acf37e84af
mallory-scotton <mallory.scotton@epitech.eu>
2026-07-28 23:03:01 +0200

    add: adding proper implementation of the link components

--- packages/ui/src/components/link/link.module.css.d.ts
+++ packages/ui/src/components/link/link.module.css.d.ts
@@ -4,6 +4,18 @@
 
 declare const styles: {
   readonly 'link': string;
+  readonly 'link--accent': string;
+  readonly 'link--arrow-none': string;
+  readonly 'link--arrow-end': string;
+  readonly 'link--arrow-start': string;
+  readonly 'link--default': string;
+  readonly 'link--is-external': string;
+  readonly 'link--label': string;
+  readonly 'link--large': string;
+  readonly 'link-arrow': string;
+  readonly 'link--small': string;
+  readonly 'link--medium': string;
+  readonly 'link--large': string;
 };
 
 export = styles;
--- packages/ui/src/components/link/link.tsx
+++ packages/ui/src/components/link/link.tsx
@@ -2,22 +2,140 @@
 // For more information, see https://graphical-playground/legal
 // mailto:support AT graphical-playground DOT com
 
-import React from 'react';
+'use client';
+
+import React, { useCallback } from 'react';
 import clsx from 'clsx';
+import { Text } from '../text';
+import NextLink from 'next/link';
+import type { LinkProps as NextLinkProps } from 'next/link';
+import { ExpandableArrow } from '../expandable-arrow';
 import styles from './link.module.css';
+
+export const linkVariants = ['default', 'accent'] as const;
+export const linkSizes = ['small', 'medium', 'large'] as const;
+export const linkArrowDirections = ['start', 'end', 'none'] as const;
+
+const LinkTextSizes = {
+  small: '100',
+  medium: '200',
+  large: '300'
+} as const;
+
+const LinkExternalIconSizes = {
+  small: 16,
+  medium: 16,
+  large: 20
+} as const;
 
 /**
  * @brief Link component props interface.
  * @see Link
  */
-export interface LinkProps extends React.HTMLAttributes<HTMLElement> {}
+export interface LinkProps extends NextLinkProps, Omit<React.ComponentPropsWithoutRef<'a'>, 'href'> {
+  /**
+   * @brief The size variations available in Link.
+   */
+  size?: (typeof linkSizes)[number];
+
+  /**
+   * @brief Position of the arrow.
+   */
+  arrowDirection?: (typeof linkArrowDirections)[number];
+
+  /**
+   * @brief Show an external link icon
+   */
+  isExternal?: boolean;
+
+  /**
+   * @brief The variant of the link.
+   */
+  variant?: (typeof linkVariants)[number];
+}
 
 /**
  * @brief Link component.
  * @details
  */
-export const Link: React.FC<LinkProps> = ({className, ...rest}) => {
-  return <div className={clsx(styles['link'], className)} {...rest} />;
+export const Link: React.FC<LinkProps> = ({
+  size = 'medium',
+  variant = 'default',
+  className,
+  children,
+  isExternal,
+  onMouseEnter,
+  onMouseLeave,
+  onFocus,
+  onBlur,
+  arrowDirection = 'none',
+  ...rest
+}) => {
+  const [isHovered, setIsHovered] = React.useState(false);
+  const [isFocused, setIsFocused] = React.useState(false);
+
+  const handleMouseEnter = useCallback(
+    (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+      setIsHovered(!isHovered);
+      onMouseEnter?.(event);
+    },
+    [onMouseEnter, isHovered]
+  );
+
+  const handleMouseLeave = useCallback(
+    (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+      setIsHovered(!isHovered);
+      onMouseLeave?.(event);
+    },
+    [onMouseLeave, isHovered]
+  );
+
+  const handleOnFocus = useCallback(
+    (event: React.FocusEvent<HTMLAnchorElement, Element>) => {
+      setIsFocused(!isFocused);
+      onFocus?.(event);
+    },
+    [onFocus, isFocused]
+  );
+
+  const handleOnBlur = useCallback(
+    (event: React.FocusEvent<HTMLAnchorElement, Element>) => {
+      setIsFocused(!isFocused);
+      onBlur?.(event);
+    },
+    [onBlur, isFocused]
+  );
+
+  return (
+    <NextLink
+      className={clsx(
+        styles.link,
+        styles[`link--${size}`],
+        styles[`link--${variant}`],
+        isExternal ? styles['link--is-external'] : styles[`link--arrow-${arrowDirection}`],
+        className
+      )}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      onFocus={handleOnFocus}
+      onBlur={handleOnBlur}
+      {...rest}
+    >
+      {arrowDirection === 'start' && !isExternal && (
+        <ExpandableArrow className={styles['link-arrow']} expanded={isHovered || isFocused} reverse hidden />
+      )}
+
+      <Text as='span' size={LinkTextSizes[size]} className={clsx(styles['link--label'])}>
+        {children}
+      </Text>
+
+      {arrowDirection === 'end' && !isExternal && (
+        <ExpandableArrow className={styles['link-arrow']} expanded={isHovered || isFocused} hidden />
+      )}
+
+      {/* {isExternal && <LinkExternalIcon size={LinkExternalIconSizes[size]} aria-label="External link" />} */}
+    </NextLink>
+  );
 };
 
 /** Set the display name for the Link component. */
