6f9bbd3372e9bd418472a5400bd71ebffe64c99c
mallory-scotton <mscotton.pro@gmail.com>
2026-08-07 15:34:12 +0200

    add: adding a new hook to allow using the forward ref or create a new one instead

---
+++ packages/ui/src/hooks/use-provided-ref-or-create.ts
@@ -0,0 +1,57 @@
+// Copyright (c) - Graphical Playground. All rights reserved.
+// For more information, see https://graphical-playground/legal
+// mailto:support AT graphical-playground DOT com
+
+import React from 'react';
+
+/**
+ * There are some situations where we only want to create a new ref if one is not provided to a component
+ * or hook as a prop. However, due to the `rules-of-hooks`, we cannot conditionally make a call to `React.useRef`
+ * only in the situations where the ref is not provided as a prop.
+ * This hook aims to encapsulate that logic, so the consumer doesn't need to be concerned with violating `rules-of-hooks`.
+ * @param providedRef The ref to use - can be a RefObject, RefCallback (functional ref), null or undefined.
+ * @type TRef The type of the RefObject which should be created.
+ * @returns A RefObject that either uses the provided ref, wraps a functional ref, or creates a new one
+ */
+export function useProvidedRefOrCreate<TRef>(
+  providedRef?: React.RefObject<TRef> | React.RefCallback<TRef> | null
+): React.RefObject<TRef> {
+  const createdRef = React.useRef<TRef | null>(null);
+  const wrapperRef = React.useRef<React.MutableRefObject<TRef | null> | null>(null);
+
+  const finalRef = React.useMemo(() => {
+    // Handle function refs
+    // We wrap it in a MutableRefObject to ensure we can set the current value
+    // and also call the callback with the latest value
+    if (typeof providedRef === 'function') {
+      if (!wrapperRef.current) {
+        let currentVal: TRef | null = null;
+
+        wrapperRef.current = {
+          get current() {
+            return currentVal;
+          },
+          set current(value: TRef | null) {
+            currentVal = value;
+
+            providedRef(value);
+          }
+        };
+      }
+
+      return wrapperRef.current;
+    }
+
+    wrapperRef.current = null;
+
+    // Return the provided ref if it's a RefObject
+    if (providedRef && typeof providedRef === 'object' && providedRef.current !== undefined) {
+      return providedRef;
+    }
+
+    // Return a new ref if it's neither a function nor a RefObject
+    return createdRef;
+  }, [providedRef, createdRef]);
+
+  return finalRef as React.RefObject<TRef>;
+}
