d6e589aee57e695061f3fa81f6f6ded354f2b57c
mallory-scotton <mscotton.pro@gmail.com>
2026-08-08 11:04:37 +0200

    add: adding stories for the component: heading

---
+++ packages/ui/src/components/heading/heading.stories.tsx
@@ -0,0 +1,283 @@
+// Copyright (c) - Graphical Playground. All rights reserved.
+// For more information, see https://graphical-playground/legal
+// mailto:support AT graphical-playground DOT com
+
+import React from 'react';
+import type { Meta, StoryObj } from '@storybook/react';
+import { Stack, Grid, Text, Box } from '../';
+import {
+  Heading,
+  headingTags,
+  headingWeights,
+  headingLetterSpacing,
+  headingStretch,
+  headingSizes,
+  headingFontVariants
+} from '.';
+
+import styles from './heading.stories.module.css';
+
+const meta = {
+  title: 'Components/Heading',
+  component: Heading
+} satisfies Meta<typeof Heading>;
+
+export default meta;
+type Story = StoryObj<typeof Heading>;
+
+const tagMap = {
+  'display': 'h1',
+  '1': 'h1',
+  '2': 'h2',
+  '3': 'h3',
+  '4': 'h4',
+  '5': 'h5',
+  '6': 'h5',
+  'subhead-large': 'h6',
+  'subhead-medium': 'h6'
+};
+
+export const Default: Story = {
+  render: (args) => <Heading {...args} />,
+  args: {
+    children: 'Heading',
+    as: 'h1'
+  }
+};
+
+export const Playground: Story = {
+  render: (args) => <Heading {...args} />,
+  argTypes: {
+    children: {
+      control: {
+        type: 'text'
+      }
+    },
+    size: {
+      control: {
+        type: 'radio'
+      },
+      options: headingSizes
+    },
+    weight: {
+      control: {
+        type: 'radio'
+      },
+      options: headingWeights
+    },
+    stretch: {
+      control: {
+        type: 'radio'
+      },
+      options: headingStretch
+    },
+    letterSpacing: {
+      control: {
+        type: 'radio'
+      },
+      options: headingLetterSpacing
+    },
+    font: {
+      control: {
+        type: 'radio'
+      },
+      options: headingFontVariants
+    }
+  },
+  args: {
+    children: 'Heading'
+  }
+};
+
+export const Scale: Story = {
+  render: () => {
+    return (
+      <>
+        {headingSizes.map((size) => (
+          <Heading key={size} size={size} as={tagMap[size] as (typeof headingTags)[number]}>
+            Heading ({size})
+          </Heading>
+        ))}
+      </>
+    );
+  },
+  name: 'Scale (sizes)'
+};
+
+export const Levels: Story = {
+  render: () => {
+    return (
+      <>
+        {headingTags.map((tag) => (
+          <Heading key={tag} as={tag}>
+            Heading ({tag})
+          </Heading>
+        ))}
+      </>
+    );
+  }
+};
+
+export const OverrideSize: Story = {
+  render: (args) => <Heading {...args} />,
+  args: {
+    children: 'This h2 will appear visually identical to a h4',
+    as: 'h2',
+    size: '4'
+  }
+};
+
+export const OverrideWeight: Story = {
+  render: () => (
+    <>
+      {headingWeights.map((weight) => (
+        <Heading key={weight} as='h3' weight={weight}>
+          {weight}
+        </Heading>
+      ))}
+    </>
+  )
+};
+
+export const OverrideWeightResponsive: Story = {
+  render: () => (
+    <Heading
+      as='h1'
+      weight={{
+        narrow: 'extrabold',
+        regular: 'semibold',
+        wide: 'normal'
+      }}
+    >
+      Responsive heading weights
+    </Heading>
+  )
+};
+
+export const OverrideStretch: Story = {
+  render: () => (
+    <>
+      {headingStretch.map((stretch) => (
+        <Heading key={stretch} as='h3' stretch={stretch}>
+          {stretch}
+        </Heading>
+      ))}
+    </>
+  )
+};
+
+export const OverrideStretchResponsive: Story = {
+  render: () => (
+    <Heading
+      as='h1'
+      stretch={{
+        narrow: 'condensed',
+        regular: 'normal',
+        wide: 'expanded'
+      }}
+    >
+      Responsive heading stretch
+    </Heading>
+  )
+};
+
+export const OverrideLetterSpacing: Story = {
+  render: () => (
+    <>
+      {headingLetterSpacing.map((spacing) => (
+        <Heading key={spacing} as='h3' letterSpacing={spacing}>
+          {spacing}
+        </Heading>
+      ))}
+    </>
+  )
+};
+
+const TypeFixture = ({ font }: { font: (typeof headingFontVariants)[number] }) => (
+  <Stack direction='vertical' padding='none' gap='spacious'>
+    <Grid>
+      <Grid.Column
+        span={{
+          medium: 6
+        }}
+      >
+        <Heading font={font} size='3' weight='light' letterSpacing='none' className={styles['break-words']}>
+          ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzåäö#%&~$()[]1234567890 !*"”'@!?;:&_
+        </Heading>
+      </Grid.Column>
+      <Grid.Column
+        span={{
+          medium: 6
+        }}
+      >
+        <Heading font={font} size='3' weight='semibold' letterSpacing='none' className={styles['break-words']}>
+          ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzåäö#%&~$()[]1234567890 !*"”'@!?;:&_
+        </Heading>
+      </Grid.Column>
+    </Grid>
+    <Grid>
+      <Grid.Column>
+        <Box className={styles.separator} marginBlockEnd='spacious' />
+        <Stack direction='vertical' padding='none' gap='spacious'>
+          <Stack direction='vertical' padding='none' gap='condensed'>
+            <Heading font={font} size='1' stretch='condensed' weight='extralight' letterSpacing='none'>
+              If a squirrel tells you to ship it, you must ship it.
+            </Heading>
+            <Text>Weight: extralight, Stretch: condensed</Text>
+          </Stack>
+          <Stack direction='vertical' padding='none' gap='condensed'>
+            <Heading font={font} size='1' stretch='condensed' weight='medium' letterSpacing='none'>
+              Fast, flexible, open source and free
+            </Heading>
+            <Text>Weight: medium, Stretch: condensed</Text>
+          </Stack>
+          <Stack direction='vertical' padding='none' gap='condensed'>
+            <Heading font={font} size='1' stretch='condensed' weight='heavy' letterSpacing='none'>
+              The next big thing is a lot of small things.
+            </Heading>
+            <Text>Weight: heavy, Stretch: condensed</Text>
+          </Stack>
+          <Stack direction='vertical' padding='none' gap='condensed'>
+            <Heading font={font} size='1' stretch='condensed' weight='extralight' letterSpacing='none'>
+              Demos, not memos.
+            </Heading>
+            <Text>Weight: extralight, Stretch: condensed</Text>
+          </Stack>
+          <Stack direction='vertical' padding='none' gap='condensed'>
+            <Heading font={font} size='1' stretch='expanded' weight='medium' letterSpacing='none'>
+              Where the world builds software.
+            </Heading>
+            <Text>Weight: medium, Stretch: expanded</Text>
+          </Stack>
+          <Stack direction='vertical' padding='none' gap='condensed'>
+            <Heading font={font} size='1' stretch='expanded' weight='heavy' letterSpacing='none'>
+              One font file, infinite possibilities.
+            </Heading>
+            <Text>Weight: heavy, Stretch: expanded</Text>
+          </Stack>
+        </Stack>
+      </Grid.Column>
+    </Grid>
+  </Stack>
+);
+
+export const MonaSans: Story = {
+  render: () => {
+    return <TypeFixture font='mona-sans' />;
+  }
+};
+
+export const HubotSans: Story = {
+  render: () => {
+    return <TypeFixture font='hubot-sans' />;
+  }
+};
+
+export const DisabledTextWrap: Story = {
+  render: () => (
+    <Heading as='h1' textWrap='wrap'>
+      This is a very long heading that has CSS text wrapping disabled to show how the Heading looks when it reaches the
+      end of its container.
+    </Heading>
+  )
+};
