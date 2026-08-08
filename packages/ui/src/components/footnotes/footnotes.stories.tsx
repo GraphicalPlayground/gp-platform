c1fa2536bf1cfee96a8f900fc7455c5b6bd41ba7
mallory-scotton <mscotton.pro@gmail.com>
2026-08-08 11:04:21 +0200

    add: adding stories for the component: footnotes

---
+++ packages/ui/src/components/footnotes/footnotes.stories.tsx
@@ -0,0 +1,54 @@
+// Copyright (c) - Graphical Playground. All rights reserved.
+// For more information, see https://graphical-playground/legal
+// mailto:support AT graphical-playground DOT com
+
+import React from 'react';
+import type { Meta, StoryObj } from '@storybook/react';
+import { Footnotes } from './footnotes';
+
+const meta = {
+  title: 'Components/Footnotes',
+  component: Footnotes,
+  args: {
+    as: 'ol',
+    visuallyHiddenHeading: 'Footnotes'
+  },
+  argTypes: {
+    as: {
+      control: {
+        type: 'inline-radio'
+      },
+      options: ['ol', 'div']
+    },
+    visuallyHiddenHeading: {
+      control: {
+        type: 'text'
+      }
+    }
+  }
+} satisfies Meta<typeof Footnotes>;
+
+export default meta;
+type Story = StoryObj<typeof Footnotes>;
+
+export const Playground: Story = {
+  render: (args) => (
+    <Footnotes {...args}>
+      <Footnotes.Item>There are now 100 million developers around the world using GitHub. </Footnotes.Item>
+      <Footnotes.Item>
+        This factor is based on data from the industry&apos;s longest running analysis of fix rates Veracode State.
+      </Footnotes.Item>
+      <Footnotes.Item>
+        Authentication with SAML single sign-on (SSO) available for organizations using GitHub Enterprise Cloud.
+      </Footnotes.Item>
+    </Footnotes>
+  )
+};
+
+export const Default: Story = {
+  render: () => (
+    <Footnotes>
+      <Footnotes.Item>There are now 100 million developers around the world using GitHub. </Footnotes.Item>
+    </Footnotes>
+  )
+};
