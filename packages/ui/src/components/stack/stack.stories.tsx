a1de64b9678fb85108b428bb421c8e0628dc4a20
mallory-scotton <mscotton.pro@gmail.com>
2026-08-08 11:05:17 +0200

    add: adding stories for the component: stack

---
+++ packages/ui/src/components/stack/stack.stories.tsx
@@ -0,0 +1,111 @@
+// Copyright (c) - Graphical Playground. All rights reserved.
+// For more information, see https://graphical-playground/legal
+// mailto:support AT graphical-playground DOT com
+
+import React from 'react';
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Stack } from '.';
+import placeholderImage from '../../fixtures/images/placeholder.png';
+
+const meta = {
+  title: 'Components/Stack',
+  component: Stack,
+  args: {
+    direction: {
+      narrow: 'vertical',
+      regular: 'vertical',
+      wide: 'horizontal'
+    },
+    gap: {
+      narrow: 'condensed',
+      regular: 'normal',
+      wide: 'spacious'
+    },
+    padding: {
+      narrow: 'condensed',
+      regular: 'normal',
+      wide: 'spacious'
+    },
+    alignItems: {
+      narrow: 'flex-start',
+      regular: 'flex-start',
+      wide: 'center'
+    },
+    justifyContent: {
+      narrow: 'space-between',
+      regular: 'space-around',
+      wide: 'space-evenly'
+    }
+  }
+} satisfies Meta<typeof Stack>;
+
+export default meta;
+type Story = StoryObj<typeof Stack>;
+
+const Template: Story = {
+  render: (args) => (
+    <Stack {...args} style={{ height: '100vh' }}>
+      <div>
+        <img
+          src={placeholderImage.src}
+          alt='placeholder with gray background and no foreground text'
+          width={300}
+          height={150}
+        />
+      </div>
+      <div>
+        <img
+          src={placeholderImage.src}
+          alt='placeholder with gray background and no foreground text'
+          width={300}
+          height={150}
+        />
+      </div>
+      <div>
+        <img
+          src={placeholderImage.src}
+          alt='placeholder with gray background and no foreground text'
+          width={300}
+          height={150}
+        />
+      </div>
+    </Stack>
+  )
+};
+
+export const Playground: Story = {
+  ...Template
+};
+
+export const Responsive: Story = {
+  ...Template,
+  args: {
+    gap: {
+      narrow: 'spacious',
+      regular: 'spacious',
+      wide: 'spacious'
+    },
+    padding: {
+      narrow: 'condensed',
+      regular: 'normal',
+      wide: 'spacious'
+    }
+  }
+};
+
+export const ResponsiveBaseScale: Story = {
+  ...Template,
+  args: {
+    gap: {
+      narrow: 8,
+      regular: 12,
+      wide: 16
+    },
+    padding: {
+      narrow: 24,
+      regular: 40,
+      wide: 80
+    }
+  }
+};
